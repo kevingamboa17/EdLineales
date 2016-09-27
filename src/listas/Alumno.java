7e6f9681d7f881d0b7b00cf06a758d95d1ddaf4d
Kevin Gamboa <kevin-gamboa1@hotmail.com>
2016-09-27 15:00:16 -0500

    Metodo insertarOrdenado

---
+++ src/listas/Alumno.java
@@ -0,0 +1,41 @@
+package listas;
+
+/**
+ * Created by kevingamboa17 on 9/27/16.
+ */
+public class Alumno {
+    String nombre;
+    String apellidos;
+    int edad;
+
+    public Alumno(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+}
--- src/listas/ListaSimple.java
+++ src/listas/ListaSimple.java
@@ -24,6 +24,71 @@ public class ListaSimple extends Lista {
         }
     }
 
+    public void insertaOrdenado(int dato){
+        if (vacio()) {
+            insertarInicio(dato);
+        }
+        else {
+            if (inicio == ultimo){
+                if (dato < (Integer)inicio.getDato()) {
+                    insertarInicio(dato);
+                }
+                else {
+                    insertarFinal(dato);
+                }
+            }
+            else {
+                Nodo antes = null, despues = inicio;
+                while (despues!= null && dato > (Integer)despues.getDato()){
+                    antes = despues;
+                    despues = despues.getSiguiente();
+                }
+                if (antes==null){
+                    insertarInicio(dato);
+                }
+                else{
+                    if (despues==null) {
+                        insertarFinal(dato);
+                    }
+                    else {
+                        Nodo nuevo = new Nodo(dato, despues);
+                        antes.setSiguiente(nuevo);
+                    }
+                }
+            }
+        }
+
+    }
+
+    public void insertaOrdenado(Alumno alumno) {
+        if (vacio())
+            insertarInicio(alumno);
+        else {
+            if (inicio == ultimo) {
+                if (alumno.getEdad() < ((Alumno)inicio.getDato()).getEdad())
+                    insertarInicio(alumno);
+                else
+                    insertarFinal(alumno);
+            } else {
+                Nodo antes = null, despues = inicio;
+                while (despues != null && alumno.getEdad() > ((Alumno) despues.getDato()).getEdad()) {
+                    antes = despues;
+                    despues = despues.getSiguiente();
+                }
+                if (antes == null) {
+                    insertarInicio(alumno);
+                } else {
+                    if (despues == null)
+                        insertarFinal(alumno);
+                    else {
+                        Nodo nuevo = new Nodo(alumno, despues);
+                        antes.setSiguiente(nuevo);
+                    }
+                }
+            }
+        }
+    }
+
     @Override
     public Object eliminaInicio() {
         Object eliminado;
@@ -38,17 +103,21 @@ public class ListaSimple extends Lista {
 
     @Override
     public Object eliminaFinal() {
-        Nodo actual = inicio;
+        Object dato = null;
+        Nodo actual = this.getInicio();
+        Nodo aux = null;
         while (actual != null){
+            aux = actual;
+            actual = actual.getSiguiente();
             if (actual.getSiguiente() == null){
+                aux.setSiguiente(null);
+                dato = actual.getDato();
                 System.out.println(actual);
-            }
-            else {
-                actual = actual.getSiguiente();
+                actual = null;
             }
 
         }
-        return null;
+        return dato;
     }
 
     public Object get(int index){
@@ -65,16 +134,32 @@ public class ListaSimple extends Lista {
 
     }
 
+
+
+    public void ImprimirAlumnos(){
+        Nodo actual = inicio;
+        while(actual != null){
+            System.out.print(((Alumno)actual.getDato()).getNombre() + " " +
+                    ((Alumno)actual.getDato()).getApellidos()  + " " +
+                    ((Alumno)actual.getDato()).getEdad() + " " +
+                    "-> ");
+            actual = actual.siguiente;
+        }
+    }
+
+
+
     public static void main(String[] args){
+        Alumno alumno1= new Alumno("Juan", "Gabriel", 10);
+        Alumno alumno2= new Alumno("Armando", "Carvajal", 74);
+        Alumno alumno3= new Alumno("marco", "Chavez", 33);
+        Alumno alumno4= new Alumno("Kevin", "Gamboa", 17);
         ListaSimple lista = new ListaSimple();
-        lista.insertarInicio(25);
-        lista.insertarInicio("Perro");
-        lista.insertarInicio(256.3);
-        lista.insertarInicio('f');
-        //lista.insertarFinal("casa");
-        lista.Imprimir();
-        lista.eliminaFinal();
-        lista.Imprimir();
-        //System.out.println("\n" + lista.get(-1));
+
+        lista.insertaOrdenado(alumno1);
+        lista.insertaOrdenado(alumno2);
+        lista.insertaOrdenado(alumno3);
+        lista.insertaOrdenado(alumno4);
+        lista.ImprimirAlumnos();
     }
 }
